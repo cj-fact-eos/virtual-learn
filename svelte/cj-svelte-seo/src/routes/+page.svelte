e5b7ec816474a7c1a69cae2f0e095bf53dd9d6ca
chandrahasJ <poojari.chandrahas@gmail.com>
2025-02-01 10:57:38 +0530

    Removed tailwind from svelte 5

--- svelte/cj-svelte-seo/src/routes/+page.svelte
+++ svelte/cj-svelte-seo/src/routes/+page.svelte
@@ -1,19 +1,3 @@
-<script lang="ts">
-    import { Button } from '$lib/components/ui/button/index.js'
-    
-    // Legacy Mode
-    // let count = 0;
-    
-    // Runes Mode
-    let count = $state(0)
 
-    function increment(){
-        count++;
-    }
-</script>
 
-<div>
-    {count}
-</div>
-
-<Button onclick={increment}>Please click me</Button>
+<a href="/counter">Go to counter</a>
---
+++ svelte/cj-svelte-seo/src/routes/counter/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { Button } from '$lib/components/ui/button/index.js'
+    
+    // Legacy Mode
+    // let count = 0;
+    
+    // Runes Mode
+    let count = $state(0)
+
+    function Decrease(){
+        if(count == 0){
+            alert("Not less than 0")
+        }
+        else {
+            count--;
+        }
+    }
+   
+</script>
+
+<div class="container">
+    <div class="shadow rounded bg-gray-900 px-6 py-2 text-2xl text-white">
+        {count}
+    </div>
+
+    <br/>
+
+    <input class="shadow rounded bg-gray-900 px-6 py-2 text-2xl text-white" type="number" bind:value={count} disabled />
+    
+    <Button class="shadow rounded bg-gray-900 px-6 py-2 text-2xl text-white" onclick={() => count++}>Increase</Button>
+    
+    <Button class="shadow rounded bg-gray-900 px-6 py-2 text-2xl text-white" onclick={() => {
+        if(count == 0){
+                alert("Not less than 0")
+            }
+            else {
+                count--;
+            }
+    }}>Decrease</Button>
+  </div> 
